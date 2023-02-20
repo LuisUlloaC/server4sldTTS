139aabedd2142e1962775bc8587d0eecf8a9c762
LuisUlloaC <113199027+LuisUlloaC@users.noreply.github.com>
2023-02-20 10:50:47 -0500

    Update urls.py

--- server4sldtts/connection/urls.py
+++ server4sldtts/connection/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import gTTs, download_file
+from .views import gTTs, download_file, b64
 
 urlpatterns = [
     path('<language>/<text>/<filename>', gTTs),
     path('descargar/<str:filename>/', download_file),
+    path('b64/<str:filename>/', b64),
 ]
